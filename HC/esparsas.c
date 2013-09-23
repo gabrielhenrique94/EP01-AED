318b830f2c11e6eaadd57ed6f2448021e54bf180
Heloisa Carbone <heloisacarbone@gmail.com>
2013-09-23 15:01:37 -0300

    Criando minha pasta do EP :D

---
+++ HC/esparsas.c
@@ -0,0 +1,90 @@
+/* Arquivo esparsas.c */
+
+#include "esparsas.h"
+#include <malloc.h>
+
+/* Coloca NUL na memoria apontada por m */
+void IniciaMatriz(PontMatriz m){
+     *m = NUL;
+}
+
+/* Libera a memoria de todos os elementos (normais e cabecas) da matriz 
+   apontada por m e coloca NUL na posicao de memoria apontada por m */
+void LiberaMatriz(PontMatriz m){
+
+/* Completar */
+
+}
+
+/* Simula 'm[i][j] = x', isto e:
+   caso o elemento m[i][j] ja exista atribuiu o valor x a ele (caso x=0, 
+      elimina este elemento da matriz);
+   caso o elmeneto m[i][j] nao exista e x!=0 insere o elemento na estrutura */
+void AtribuiMatriz(PontMatriz m, int i, int j, float x){
+
+/* Completar */
+
+}
+
+/* Devolve o equivalente a 'm[i][j]' */
+float ValorMatriz(Matriz m, int i, int j){
+
+/* Completar */
+
+   return 0;
+}
+
+/* Retorna as dimensoes maximas de 'm' nas memorias apontadas em 
+   'l' (numero de linhas) e 'c' (numero de colunas). Se m==NUL retorna 0 e 0 nas
+   memorias apontadas por l e c */
+void OrdemMatriz(Matriz  m, int* l, int* c){
+
+/* Completar */
+
+}
+
+/* Devolve o numero total de nos (elementos do tipo EleMatriz) alocados para 
+   representar a matriz esparsa apontada por 'm' */
+int NumeroNos(Matriz m){
+
+/* Completar */
+
+   return 0;
+}
+
+/* Devolve o numero total de cabecalhos (elementos do tipo CabMatriz) alocados 
+   para representar a matriz esparsa apontada por 'm' */
+int NumeroCabecalhos(Matriz m){
+
+/* Completar */
+
+   return 0;
+}
+
+/* Devolve o tamanho em bytes de um nó (ElemMatriz) usado na representação */
+int TamanhoNo(){
+
+/* Completar */
+
+   return 0;
+}
+
+/* Devolve o tamanho em bytes de um cabecalho (CabMatriz) usado na representação */
+int TamanhoCabecalho(){
+
+/* Completar */
+
+   return 0;
+}
+
+/* Devolve o endereco do primeiro PontCab de uma nova matriz correspondendo a 
+   soma das matrizes apontadas por a e b: a+b.
+   Tente fazer uma implementacao eficiente (nao faz sentido tentar somar dois
+   elementos que nao existem (que teriam valor zero) na matriz. */
+Matriz SomaMatriz(Matriz a, Matriz b) {
+
+/* Completar */
+
+   return NUL;    
+}
+ 
---
+++ HC/testaesparsas.c
@@ -0,0 +1,84 @@
+/* Arquivo testaesparsas.c */
+#include <stdio.h>
+#include "esparsas.c"
+
+void LeMatriz(PontMatriz a) {
+/* Inicializa e lê os elementos de 'a' */
+  int i,j;
+  float x;
+  IniciaMatriz(a);
+  do {
+   scanf("%d %d %f",&i,&j,&x);
+   if (i!=0)
+     AtribuiMatriz(a,i,j,x);
+  } while (i!=0);
+}
+
+void ImprimirListaDeCabecalhos(Matriz a){
+  if (!a) return;
+  PontCab c1 = a;
+  PontElem e;
+  printf("Indices das Colunas: %d", c1->coluna);
+  c1 = c1->direita;
+  while (c1){
+    printf("->%d", c1->coluna);
+    c1 = c1->direita;
+  }
+  printf("\n");
+}
+
+void EscreveMatriz(Matriz a) {
+  int m,n,i,j;
+  float x;
+  OrdemMatriz(a,&m,&n);
+  printf("Linha Maxima: %3d , Coluna Maxima: %3d\n",m,n);
+  printf("Elementos Normais: %3d , Elementos Cabecalho: %3d\n",NumeroNos(a), NumeroCabecalhos(a));
+  PontCab c = a;
+  PontElem e;
+  while (c){
+    e = c->abaixo;
+    while (e){
+      printf("%3d %3d %6.1f\n",e->linha,c->coluna,e->valor);      
+      e = e->abaixo;
+    }
+    c = c->direita;
+  }      
+}
+
+float Custo(Matriz a) {
+/* Calcula o custo em uso de memoria da representação esparsa em
+   comparação com a usual, em porcentagem */
+  int m,n;
+  OrdemMatriz(a,&m,&n);
+  if (m*n!=0)
+    return 100.0*(NumeroNos(a)*TamanhoNo()+NumeroCabecalhos(a)*TamanhoCabecalho())
+                  /(m*n*sizeof(float));
+  else
+    return 100;
+}
+
+int main() {
+  Matriz a,b,c;
+  LeMatriz(&a);
+  EscreveMatriz(a);
+  printf("Custo: %6.3f%%\n",Custo(a));
+  ImprimirListaDeCabecalhos(a);
+  LeMatriz(&b);
+  EscreveMatriz(b);
+  printf("Custo: %6.3f%%\n",Custo(b));
+  ImprimirListaDeCabecalhos(b);
+  c = SomaMatriz(a,b);
+  if (c==NUL) 
+    printf("A matriz soma esta vazia\n");
+  else {
+    EscreveMatriz(c);
+    printf("Custo: %6.3f%%\n",Custo(c));
+    ImprimirListaDeCabecalhos(c);    
+    LiberaMatriz(&c);
+  }
+  LiberaMatriz(&a);
+  LiberaMatriz(&b);
+  printf("\n");
+//  system("pause");
+  return 0;
+}
